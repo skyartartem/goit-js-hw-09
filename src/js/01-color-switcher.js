f0b515ce3e858d96f2b266b36399ef08769b2581
Artem Riabchych <artemskyart@gmail.com>
2022-12-23 11:15:42 +0200

    firs-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const bodyRef = document.querySelector("body");
+stopBtn.disabled = true;
 
+let timerId = null;
+
+startBtn.addEventListener("click", startChangingColor);
+
+stopBtn.addEventListener("click", stopChangingColor);
+
+function startChangingColor() {
+
+  timerId = setInterval(() => {
+      bodyRef.style.backgroundColor = getRandomHexColor();
+      console.log(`Body color is ${bodyRef.style.backgroundColor}`);
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+  }, 1000);
+
+}
+
+function stopChangingColor() {
+
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
